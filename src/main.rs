37b5ce45e54155b96ffebfe4f967f58c9e1be621
Chris Covert <chris.covert@blackboard.com>
2022-01-07 18:15:20 -0500

    add basic CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser};
+
+/// A tool for helping check out git branches using fuzzy search
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    /// If provided, remote branches will be searched instead
+    #[clap(short, long)]
+    remote: bool,
+
+    /// The default search string to use
+    query: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.remote {
+        println!("Remote!");
+    }
+
+    if let Some(query) = cli.query {
+        println!("{}", query);
+    }
+
+    println!("That's all!");
 }
