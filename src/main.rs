41457b6467bdeb565b81a6fe06f60c4fb249a7a0
Chris Covert <chris.covert@blackboard.com>
2022-01-07 19:23:54 -0500

    feat: print out branch names

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use clap::{Parser};
+use anyhow::{Context, Result};
+use clap::Parser;
+use git2::{BranchType, Repository};
+use std::env;
 
 /// A tool for helping check out git branches using fuzzy search
 #[derive(Parser)]
@@ -9,19 +12,49 @@ struct Cli {
     remote: bool,
 
     /// The default search string to use
-    query: Option<String>
+    query: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    if cli.remote {
-        println!("Remote!");
-    }
+    let cwd = env::current_dir().context("Couldn't determine current working directory. Do you have permissions, or was the directory deleted?")?;
+    let repo =
+        Repository::open(cwd).context("Couldn't find a git repo in the current directory.")?;
 
-    if let Some(query) = cli.query {
-        println!("{}", query);
-    }
+    let branch_type = if cli.remote {
+        BranchType::Remote
+    } else {
+        BranchType::Local
+    };
 
-    println!("That's all!");
+    let branches = repo
+        .branches(Some(branch_type))
+        .context("Error listing branches in the git repo")?
+        .collect::<Result<Vec<_>, _>>()
+        .context("Processing one of the repo's branches caused an error")?;
+
+    let branch_names = branches
+        .iter()
+        .map(|(branch, _)| {
+            branch
+                .name()
+                .unwrap_or(None)
+                .context("Failed to parse a branch name into a UTF8 string")
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    println!("{:?}", branch_names);
+
+    // let branch_errors  = branches_result.filter_map(|res| res.err());
+
+    // // TODO: find a better way to handle these seemingly unlikely errors
+    // branch_errors.for_each(|err| println!("There was an error processing a branch: {:?}", err));
+
+    // branches_result.for_each(|result| match result {
+    //     Ok((branch, _)) => println!("{:?}", branch.name()),
+    //     Err(err) => println!("ERROR: {:?}", err),
+    // });
+
+    Ok(())
 }
