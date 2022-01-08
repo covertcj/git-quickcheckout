fba49dae50e1dfe4c75b339f3a2b50ce41fd3490
Christopher J Covert <covertops5@gmail.com>
2022-01-07 23:56:01 -0500

    basic UI/branches layout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
 use git2::{BranchType, Repository};
-use std::env;
+use std::{
+    env,
+    io::{self},
+};
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::Style,
+    text::{Span, Spans},
+    widgets::{Block, Borders, List, ListItem},
+    Terminal,
+};
 
 /// A tool for helping check out git branches using fuzzy search
 #[derive(Parser)]
@@ -15,14 +31,22 @@ struct Cli {
     query: Option<String>,
 }
 
-fn main() -> Result<()> {
-    let cli = Cli::parse();
+struct App {
+    input: String,
+    pub entries: Option<Vec<String>>,
+}
 
-    let cwd = env::current_dir().context("Couldn't determine current working directory. Do you have permissions, or was the directory deleted?")?;
-    let repo =
-        Repository::open(cwd).context("Couldn't find a git repo in the current directory.")?;
+impl App {
+    fn default() -> Result<Self> {
+        Ok(Self {
+            input: "".to_string(),
+            entries: None,
+        })
+    }
+}
 
-    let branch_type = if cli.remote {
+fn load_branches(repo: &Repository, remotes: bool) -> Result<Vec<String>> {
+    let branch_type = if remotes {
         BranchType::Remote
     } else {
         BranchType::Local
@@ -40,11 +64,69 @@ fn main() -> Result<()> {
             branch
                 .name()
                 .unwrap_or(None)
+                .map(|s| s.to_string())
                 .context("Failed to parse a branch name into a UTF8 string")
         })
         .collect::<Result<Vec<_>, _>>()?;
 
-    println!("{:?}", branch_names);
+    Ok(branch_names)
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend).context("Failed to attach to terminal.")?;
+
+    let mut app = App::default().context("Failed to initialize TUI app.")?;
+
+    let cwd = env::current_dir().context("Couldn't determine current working directory. Do you have permissions, or was the directory deleted?")?;
+    let repo =
+        Repository::open(cwd).context("Couldn't find a git repo in the current directory.")?;
+
+    app.entries = Some(load_branches(&repo, cli.remote)?);
+
+    loop {
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Char('q') = key.code {
+                break;
+            }
+        }
+
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Min(1), Constraint::Length(3)].as_ref())
+                .split(f.size());
+
+            let branch_items: Vec<ListItem> = if let Some(entries) = &app.entries {
+                entries
+                    .iter()
+                    .map(|entry| ListItem::new(Spans::from(Span::styled(entry, Style::default()))))
+                    .collect()
+            } else {
+                vec![]
+            };
+
+            let branches_block = Block::default().title("Branches").borders(Borders::ALL);
+            let branches_list = List::new(branch_items).block(branches_block);
+
+            let search_block = Block::default().title("Search").borders(Borders::ALL);
+
+            f.render_widget(branches_list, chunks[0]);
+            f.render_widget(search_block, chunks[1]);
+        })?;
+    }
+
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
 
     Ok(())
 }
